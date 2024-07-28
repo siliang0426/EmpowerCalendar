0dfe426b915a3080b42b3908fef816754809d932
zuxiangw <zuxiangwang2002@gmail.com>
2024-07-28 18:24:42 -0400

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,21 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import { Toaster } from "react-hot-toast";
 import Landing from "./components/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Toaster />
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth">
+            <Route path="sign-in" element={<Login />} />
+            <Route path="sign-up" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
